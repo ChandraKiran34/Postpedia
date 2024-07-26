703bcd24d69290cda144579645a63afa2d93628f
ChandraKiran34 <bchakri06@gmail.com>
2024-07-26 20:49:35 +0530

    postpedia

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import {BrowserRouter,Navigate,Routes,Route} from 'react-router-dom';
+import HomePage from './scenes/homePage/index.jsx'
+import LoginPage from './scenes/loginPage/index.jsx';
+import ProfilePage from './scenes/profilePage/index.jsx';
+import {useMemo} from 'react';
+import {  useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode])
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline />
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/profile/:userId' element={<ProfilePage/>}/>
+      </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
